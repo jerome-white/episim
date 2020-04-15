40295f1968513301a650d21a3a9aa811e5c5be56
Jerome White <jerome@thewex.net>
2020-04-15 18:22:18 +0000

    Departure event refactored to its own function
    Facilitates calling once init is finished.

--- src/util.c
+++ src/util.c
@@ -32,3 +32,44 @@ void lp_log(const char *etype,
 
   return;
 }
+
+int human_departure_events(struct state *s, tw_lp *lp) {
+  int i, sent;
+  unsigned int norm_calls;
+  double travel_time;
+  long double people;
+  tw_stime ts;
+  tw_event *event;
+  struct message *msg;
+  struct population travelers;
+  struct transition *movement;
+
+  memset((struct population *)&travelers, 0, sizeof(struct population));
+
+  for (i = 0, sent = 0; i < __tiles; i++) {
+    norm_calls = 0;
+    movement = &s->movement[i];
+    people = tw_rand_normal_sd(lp->rng,
+			       movement->mean,
+			       movement->deviation,
+			       &norm_calls);
+    travelers.susceptible = ROSS_MIN(s->people.susceptible, people);
+    if (travelers.susceptible > 0) {
+      s->people = population_decrease(&s->people, &travelers);
+
+      travel_time = movement->distance / HUMAN_TRAVEL_SPEED;
+      ts = tw_rand_exponential(lp->rng, travel_time);
+      event = tw_event_new(i, ts, lp);
+
+      msg = (struct message *)tw_event_data(event);
+      msg->etype = HUMAN_ARRIVAL_EVENT;
+      msg->rng_calls = norm_calls + 1;
+      msg->people = travelers;
+
+      tw_event_send(event);
+      sent += 1;
+    }
+  }
+
+  return sent;
+}
