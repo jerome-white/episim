55c179b4ea99c63b196976b809462f58daeea5c3
Jerome White <jerome@thewex.net>
2020-04-25 11:26:06 +0530

    Parallelize figure creation

--- analysis/dynamics.py
+++ analysis/dynamics.py
@@ -4,6 +4,7 @@ import logging
 from scipy import constants
 from pathlib import Path
 from argparse import ArgumentParser
+from multiprocessing import Pool, JoinableQueue
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -24,51 +25,71 @@ class TimeConversion:
         diff = x[self.rtime] - x[self.rtime].min()
         return diff.apply(lambda x: x * constants.milli / constants.day)
 
+def func(queue, by, rtime, output):
+    gridspec_kw = {
+        'hspace': 0.1,
+        'height_ratios': [1, 0.25],
+    }
+
+    while True:
+        (name, group) = queue.get()
+        logging.info(name)
+
+        (fig, (top, bottom)) = plt.subplots(nrows=2,
+                                            sharex=True,
+                                            gridspec_kw=gridspec_kw)
+
+        view = (group
+                .drop(columns=[rtime, by])
+                .set_index('offset')
+                .sort_index())
+        view.plot(grid=True, ax=top)
+        top.set_ylabel('People')
+
+        view = (view
+                .sum(axis='columns')
+                .to_frame()
+                .apply(lambda x: (x - x.iloc[0]) / x.iloc[0]))
+        view.plot(grid=True, legend=False, ax=bottom)
+        bottom.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
+        bottom.set_ylabel('Pop. chg')
+
+        fname = output.joinpath('{:04d}'.format(name)).with_suffix('.png')
+        plt.xlabel('Days')
+        plt.suptitle('Tile {}'.format(name))
+        plt.savefig(fname)
+        plt.close(fig)
+
+        queue.task_done()
+
 arguments = ArgumentParser()
 arguments.add_argument('--master', type=int, default=0)
 arguments.add_argument('--output', type=Path)
+arguments.add_argument('--workers', type=int)
 args = arguments.parse_args()
 
 by = 'lp'
 rtime = 'virtual_time'
-usecols = [
-    rtime,
+queue = JoinableQueue()
+initargs = (
+    queue,
     by,
-    'susceptible',
-    'infected',
-    'recovered',
-]
-df = (pd
-      .read_csv(sys.stdin, usecols=usecols)
-      .assign(offset=TimeConversion(rtime)))
+    rtime,
+    args.output,
+)
 
-gridspec_kw = {
-    'hspace': 0.1,
-    'height_ratios': [1, 0.25],
-}
+with Pool(args.workers, func, initargs):
+    usecols = [
+        rtime,
+        by,
+        'susceptible',
+        'infected',
+        'recovered',
+    ]
+    df = (pd
+          .read_csv(sys.stdin, usecols=usecols)
+          .assign(offset=TimeConversion(rtime)))
 
-for (n, g) in df.groupby(by, sort=False):
-    (fig, (top, bottom)) = plt.subplots(nrows=2,
-                                        sharex=True,
-                                        gridspec_kw=gridspec_kw)
-
-    view = (g
-            .drop(columns=[rtime, by])
-            .set_index('offset')
-            .sort_index())
-    view.plot(grid=True, ax=top)
-    top.set_ylabel('People')
-
-    view = (view
-            .sum(axis='columns')
-            .to_frame()
-            .apply(lambda x: (x - x.iloc[0]) / x.iloc[0]))
-    view.plot(grid=True, legend=False, ax=bottom)
-    bottom.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
-    bottom.set_ylabel('Pop. chg')
-
-    fname = args.output.joinpath('{:04d}'.format(n)).with_suffix('.png')
-    plt.xlabel('Days')
-    plt.suptitle('Tile {}'.format(n))
-    plt.savefig(fname)
-    plt.close(fig)
+    for i in df.groupby(by, sort=False):
+        queue.put(i)
+    queue.join()
